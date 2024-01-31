73ec482f34e856d40822d2252deb700e1fb9b543
Toshiba <Toshiba@DESKTOP-H1F6LO0>
2024-01-31 11:33:25 -0600

    Agregar archivos de proyecto.

---
+++ Eje1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine(" INGRESE UN NUMERO ");
+
+if (int.TryParse(Console.ReadLine(), out int N))
+{
+    Console.WriteLine(" HAS INTRODUCIDO EL NUMERO "+N+" GRACIAS ");
+}
+else
+{
+    Console.WriteLine(" POR FAVOR INGRESE UN NUMERO ENTERO...");
+}
+Console.ReadKey();
---
+++ Eje2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine(" ¿CUANTOS AÑOS TIENES? ");
+
+if (int.TryParse(Console.ReadLine(), out int Edad))
+{
+    Console.WriteLine(" AHORA SE QUE EDAD TIENES " + Edad + " AÑOS, GRACIAS ");
+    
+}   
+else
+{
+    Console.WriteLine(" POR FAVOR INGRESE UNA EDAD REAL...");
+}
+Console.ReadKey();
